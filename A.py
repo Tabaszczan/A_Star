ae26deb1dd05ac3d8450772964beafb6d52275b1
PodolakM <mati.pooo@gmail.com>
2019-10-27 21:03:48 +0100

    Zrobiona A*, wynik zapisywany do nowego pliku, ewentualnie podzielic na mniejsze funkcje

--- A.py
+++ A.py
@@ -1,11 +1,12 @@
+import csv
 with open("grid.txt", "r") as f:
     mapa = f.readlines()
 
 for index in range(len(mapa)):
     mapa[index] = list(map(int, mapa[index].split(" ")))
 
-start = (3, 5)
-meta = (0, 0)
+start = (19, 0)
+meta = (0, 19)
 
 
 class Pole:
@@ -16,9 +17,21 @@ class Pole:
         self.g = g
         self.fun = 0
 
+    def __eq__(self, other):
+        if self.pozycja == other.pozycja:
+            return True
+        return False
+
 
 def heurestyka(poz, g, cel):
     return g + ((poz[0] - cel[0]) ** 2 + (poz[1] - cel[1]) ** 2) ** (1 / 2)
+
+
+def czyistnieje(punkt, lista):
+    for i in lista:
+        if i.pozycja == punkt:
+            return True
+    return False
 
 
 def astar(punktstart, punktkonc, plansza):
@@ -40,23 +53,45 @@ def astar(punktstart, punktkonc, plansza):
                 continue
             if nowapozycja == poczatek.pozycja:
                 continue
+            if czyistnieje(nowapozycja, lista_z):
+                continue
+            if czyistnieje(nowapozycja, lista_o):
+                continue
             nowy = Pole(nowapozycja, lista_z[-1], lista_z[-1].g)
-           # for i in lista_o:
-              #  if i.pozycja == nowy.pozycja:
             lista_o.append(nowy)
             lista_o[-1].g += 1
             lista_o[-1].fun = heurestyka(lista_o[-1].pozycja, lista_o[-1].g, koniec.pozycja)
-        minimum = lista_o[-1]
-        for i in lista_o:
-            if i.fun < minimum.fun:
-                minimum = i
+        if len(lista_o) == 0:
+            return 'BRAK SCIEZKI'
+        minimum = lista_o[0]
+        numer = 0
+        for index, elem in enumerate(lista_o):
+
+            if elem.fun < minimum.fun:
+                minimum = elem
+                numer = index
         lista_z.append(minimum)
-        for i in lista_o:
+        lista_o.pop(numer)
+        for i in lista_z:
             if i.pozycja == koniec.pozycja:
                 sciezka = []
-                for i in lista_z:
-                   sciezka.append(i.pozycja)
-                return sciezka[::-1]
+                rodzic = lista_z[-1]
+                while rodzic is not None:
+                    sciezka.append(rodzic.pozycja)
+                    rodzic = rodzic.rodzic
+                return sciezka
 
 
-print(astar(start, meta, mapa))
+
+droga = astar(start, meta, mapa)
+
+if droga == 'BRAK SCIEZKI':
+    print(droga)
+else:
+    for i in range(len(droga)):
+        mapa[droga[i][0]][droga[i][1]] = 3
+    for i in range(len(mapa)):
+        print(mapa[i])
+    with open("gridrozwiaz.txt", "w") as f:
+        w = csv.writer(f, delimiter=' ', lineterminator='\n')
+        w.writerows(mapa)
